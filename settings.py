259034d166d6daf7f3614b19a1a7a6166e74baa1
Andy Wang <cbeuw.andy@gmail.com>
2021-12-04 15:58:53 +0000

    Update thumbnail link
    Signed-off-by: Andy Wang <cbeuw.andy@gmail.com>

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 SOLR_SELECT_ENDPOINT = "http://localhost:8983/solr/felix_archive/select"
 FELIX_ARCHIVE_LINK = "https://f001.backblazeb2.com/file/felixonlinearchive/issues/pdfs/felix_{}.pdf"
-THUMBNAIL_LINK = "https://f001.backblazeb2.com/file/felixonlinearchive/issues/thumbnails/felix_{}_small.png"
+THUMBNAIL_LINK = "https://f001.backblazeb2.com/file/felixonlinearchive/issues/thumbnails/felix_{}.jpg"
 GENERATED_LISTING_DIR = "generated"
 FELIX_DATES_CSV = "https://raw.githubusercontent.com/FelixOnline/FelixArchiveBuilder/master/felix_dates.csv"
 SOLR_USERNAME = ""
