de440b588a353be4cb8b1191758a5b090b735304
Timothy Langer <zeevox.dev@gmail.com>
2024-11-12 22:49:48 +0000

    fix: update issues link to point to new URL

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 SOLR_CORE_NAME = "felix_archive"
 SOLR_SELECT_ENDPOINT = f"http://solr:8983/solr/{SOLR_CORE_NAME}/select"
-FELIX_ARCHIVE_LINK = "https://f001.backblazeb2.com/file/felixonlinearchive/issues/pdfs/felix_{}.pdf"
+FELIX_ARCHIVE_LINK = "https://issues.felixonline.co.uk/felix_{}.pdf"
 THUMBNAIL_LINK = "https://f001.backblazeb2.com/file/felixonlinearchive/issues/thumbnails/felix_{}.jpg"
 GENERATED_LISTING_DIR = "generated"
 FELIX_DATES_CSV = "https://raw.githubusercontent.com/FelixOnline/FelixArchiveBuilder/master/felix_dates.csv"
